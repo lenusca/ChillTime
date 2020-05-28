2438727ac628b77c742fed64745b1aba94bb55df
anamiz <ana.maia97@hotmail.com>
2020-05-28 23:14:23 +0100

    DashBoardUser

--- App/app/src/main/java/com/example/chilltime/DashboardUser.java
+++ App/app/src/main/java/com/example/chilltime/DashboardUser.java
@@ -1,5 +1,7 @@
 package com.example.chilltime;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +9,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DashboardUser extends Fragment {
+
+    private TextView name, monthsTT, daysTT, hoursTT, monthsMovie, daysMovie, hoursMovie, monthsSeries, daysSeries, hoursSeries;
+    private ImageView photo;
+    private PieChart movieChart, seriesChart;
+    View view;
+
 
     public DashboardUser() {
         // Required empty public constructor
@@ -22,7 +42,94 @@ public class DashboardUser extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard_user, container, false);
+        view = inflater.inflate(R.layout.fragment_dashboard_user, container, false);
+        name = view.findViewById(R.id.name);
+        photo = view.findViewById(R.id.photo);
+        monthsTT = view.findViewById(R.id.monthsTT);
+        daysTT = view.findViewById(R.id.daysTT);
+        hoursTT = view.findViewById(R.id.hoursTT);
+        monthsMovie = view.findViewById(R.id.monthsMovies);
+        daysMovie = view.findViewById(R.id.daysMovies);
+        hoursMovie = view.findViewById(R.id.hoursMovies);
+        monthsSeries = view.findViewById(R.id.monthsSeries);
+        daysSeries = view.findViewById(R.id.daysSeries);
+        hoursSeries = view.findViewById(R.id.hoursSeries);
+
+
+
+        //MOVIES CHART
+        movieChart = view.findViewById(R.id.moviesChart);
+        movieChart.setUsePercentValues(true);
+        movieChart.setHoleRadius(0f);
+        movieChart.setTransparentCircleRadius(0f);
+
+        //SERIES CHART
+        seriesChart = view.findViewById(R.id.seriesChart);
+        seriesChart.setUsePercentValues(true);
+        seriesChart.setHoleRadius(0f);
+        seriesChart.setTransparentCircleRadius(0f);
+        Description desc = new Description();
+        desc.setText("");
+
+
+
+        //Example with fake numbers
+        name.setText("Joao Pedro");
+        String[] totalTime = transformTime(100000);
+        monthsTT.setText(totalTime[0]);
+        daysTT.setText(totalTime[1]);
+        hoursTT.setText(totalTime[2]);
+
+        String[] moviesTime = transformTime(100000);
+        monthsMovie.setText(moviesTime[0]);
+        daysMovie.setText(moviesTime[1]);
+        hoursMovie.setText(moviesTime[2]);
+
+        String[] seriesTime = transformTime(1000);
+        monthsSeries.setText(seriesTime[0]);
+        daysSeries.setText(seriesTime[1]);
+        hoursSeries.setText(seriesTime[2]);
+
+
+        //Example with fake numbers for MOVIE CHART
+        //IMPORTANT : 40f means 40%//
+        List<PieEntry> value = new ArrayList<>();
+        value.add(new PieEntry(40f, "Horror"));
+        value.add(new PieEntry(60f, "Drama"));
+
+
+        PieDataSet pieDataSet = new PieDataSet(value, "");
+        PieData pieData = new PieData(pieDataSet);
+        movieChart.setData(pieData);
+        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        movieChart.setDescription(desc);
+
+
+
+        //Example with fake numbers for SERIES CHART
+        List<PieEntry> valueSeries = new ArrayList<>();
+        valueSeries.add(new PieEntry(40f, "Thriller"));
+        valueSeries.add(new PieEntry(60f, "Drama"));
+
+
+        PieDataSet pieDataSetSeries = new PieDataSet(valueSeries, "");
+        PieData pieDataSeries = new PieData(pieDataSetSeries);
+        seriesChart.setData(pieDataSeries);
+        pieDataSetSeries.setColors(ColorTemplate.JOYFUL_COLORS);
+        seriesChart.setDescription(desc);
+
+
+        return view;
+    }
+
+    private String[] transformTime(int minutes){
+        int minutesInAMonth = 43829;
+        int minutesInADay = 1440;
+        int months = minutes / minutesInAMonth;
+        int days = (minutes - months * minutesInAMonth) / minutesInADay;
+        int hours = (minutes - days * minutesInADay - months * minutesInAMonth) / 60;
+        String[] res = {String.valueOf(months), String.valueOf(days), String.valueOf(hours)};
+        System.out.println(res);
+        return res;
     }
 }
