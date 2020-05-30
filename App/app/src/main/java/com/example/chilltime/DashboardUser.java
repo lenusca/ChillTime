bd01c7cc6f908b7e8672f1ac4f7dc406a3a2fcf8
lenusca <helenamncardoso@gmail.com>
2020-05-31 00:28:25 +0100

    some changes in front end of dashboard user and getting user time

--- App/app/src/main/java/com/example/chilltime/DashboardUser.java
+++ App/app/src/main/java/com/example/chilltime/DashboardUser.java
@@ -1,10 +1,13 @@
 package com.example.chilltime;
 
+import android.app.Activity;
 import android.content.Context;
 import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +21,12 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,10 +37,16 @@ import java.util.List;
  */
 public class DashboardUser extends Fragment {
 
-    private TextView name, monthsTT, daysTT, hoursTT, monthsMovie, daysMovie, hoursMovie, monthsSeries, daysSeries, hoursSeries;
-    private ImageView photo;
+    private TextView monthsTT, daysTT, hoursTT, monthsMovie, daysMovie, hoursMovie, monthsSeries, daysSeries, hoursSeries;
+
     private PieChart movieChart, seriesChart;
     View view;
+
+    // Firebase
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
+    String userID;
+    int mTime, sTime, tvTime;
 
 
     public DashboardUser() {
@@ -43,8 +58,6 @@ public class DashboardUser extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_dashboard_user, container, false);
-        name = view.findViewById(R.id.name);
-        photo = view.findViewById(R.id.photo);
         monthsTT = view.findViewById(R.id.monthsTT);
         daysTT = view.findViewById(R.id.daysTT);
         hoursTT = view.findViewById(R.id.hoursTT);
@@ -55,7 +68,91 @@ public class DashboardUser extends Fragment {
         daysSeries = view.findViewById(R.id.daysSeries);
         hoursSeries = view.findViewById(R.id.hoursSeries);
 
+        //FIREBASE
+        mAuth = FirebaseAuth.getInstance();
+        mStore = FirebaseFirestore.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+        // ir buscar o documento relacionado com o utilizador, usando o uid
+        final DocumentReference documentReference = mStore.collection("Users").document(userID);
+        documentReference.addSnapshotListener((Activity) getContext(), new EventListener<DocumentSnapshot>(){
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                mTime = (documentSnapshot.getLong("WatchesMoviesTime")).intValue();
+                sTime = (documentSnapshot.getLong("WatchesSeriesTime")).intValue();
+                tvTime = mTime + sTime;
 
+                System.out.println(mTime);
+                System.out.println(tvTime);
+                // Tempo total
+                String[] totalTime = transformTime(tvTime);
+                if(totalTime[0].length() == 1){
+                    monthsTT.setText("0"+totalTime[0]);
+                }
+                else{
+                    monthsTT.setText(totalTime[0]);
+                }
+
+                if(totalTime[1].length() == 1){
+                    daysTT.setText("0"+totalTime[1]);
+                }
+                else{
+                    daysTT.setText(totalTime[1]);
+                }
+
+                if(totalTime[2].length() == 1){
+                    hoursTT.setText("0"+totalTime[2]);
+                }
+                else{
+                    hoursTT.setText(totalTime[2]);
+                }
+
+                // Tempo de filmes
+                String[] moviesTime = transformTime(mTime);
+                if(moviesTime[0].length() == 1){
+                    monthsMovie.setText("0"+moviesTime[0]);
+                }
+                else{
+                    monthsMovie.setText(moviesTime[0]);
+                }
+
+                if(moviesTime[1].length() == 1){
+                    daysMovie.setText("0"+moviesTime[1]);
+                }
+                else{
+                    daysMovie.setText(moviesTime[1]);
+                }
+
+                if(moviesTime[2].length() == 1){
+                    hoursMovie.setText("0"+moviesTime[2]);
+                }
+                else{
+                    hoursMovie.setText(moviesTime[2]);
+                }
+
+                // Tempo de séries
+                String[] seriesTime = transformTime(sTime);
+                if(seriesTime[0].length() == 1){
+                    monthsSeries.setText("0"+seriesTime[0]);
+                }
+                else{
+                    monthsSeries.setText(seriesTime[0]);
+                }
+
+                if(seriesTime[1].length() == 1){
+                    daysSeries.setText("0"+seriesTime[1]);
+                }
+                else{
+                    daysSeries.setText(seriesTime[1]);
+                }
+
+                if(seriesTime[2].length() == 1){
+                    hoursSeries.setText("0"+seriesTime[2]);
+                }
+                else{
+                    hoursSeries.setText(seriesTime[2]);
+                }
+            }
+        });
 
         //MOVIES CHART
         movieChart = view.findViewById(R.id.moviesChart);
@@ -73,22 +170,7 @@ public class DashboardUser extends Fragment {
 
 
 
-        //Example with fake numbers
-        name.setText("Joao Pedro");
-        String[] totalTime = transformTime(100000);
-        monthsTT.setText(totalTime[0]);
-        daysTT.setText(totalTime[1]);
-        hoursTT.setText(totalTime[2]);
 
-        String[] moviesTime = transformTime(100000);
-        monthsMovie.setText(moviesTime[0]);
-        daysMovie.setText(moviesTime[1]);
-        hoursMovie.setText(moviesTime[2]);
-
-        String[] seriesTime = transformTime(1000);
-        monthsSeries.setText(seriesTime[0]);
-        daysSeries.setText(seriesTime[1]);
-        hoursSeries.setText(seriesTime[2]);
 
 
         //Example with fake numbers for MOVIE CHART
@@ -102,7 +184,7 @@ public class DashboardUser extends Fragment {
         PieData pieData = new PieData(pieDataSet);
         movieChart.setData(pieData);
         pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
-        movieChart.setDescription(desc);
+        //movieChart.setDescription(desc);
 
 
 
@@ -116,8 +198,7 @@ public class DashboardUser extends Fragment {
         PieData pieDataSeries = new PieData(pieDataSetSeries);
         seriesChart.setData(pieDataSeries);
         pieDataSetSeries.setColors(ColorTemplate.JOYFUL_COLORS);
-        seriesChart.setDescription(desc);
-
+        //seriesChart.setDescription(desc);
 
         return view;
     }
@@ -129,7 +210,6 @@ public class DashboardUser extends Fragment {
         int days = (minutes - months * minutesInAMonth) / minutesInADay;
         int hours = (minutes - days * minutesInADay - months * minutesInAMonth) / 60;
         String[] res = {String.valueOf(months), String.valueOf(days), String.valueOf(hours)};
-        System.out.println(res);
         return res;
     }
 }
